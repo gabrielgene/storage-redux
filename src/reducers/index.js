b1252c7555360a2a982d9dde401e5dfee4782af5
Gabriel Gene <gabrielgene97@gmail.com>
2017-07-12 23:10:52 -0300

    Implement reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,19 @@
-import { combineReducers } from 'redux'
+import {
+  ADD_STORAGE,
+  REMOVE_STORAGE
+} from '../actions'
 
-function storage(state = [], action) {
+
+export default function storage(state = [], action) {
   switch (action.type) {
-    case
+    case ADD_STORAGE:
+      return [
+        ...state,
+         action.item
+      ]
+    case REMOVE_STORAGE:
+      return state.filter( obj => obj.id !== 1)
+    default:
+      return state
   }
 }
