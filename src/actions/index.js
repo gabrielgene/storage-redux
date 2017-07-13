4d06577931f21e1017542856e34cb9be85460b15
Gabriel Gene <gabrielgene97@gmail.com>
2017-07-12 23:10:21 -0300

    Create actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,11 @@
 export const ADD_STORAGE = 'ADD_STORAGE'
 
+export const REMOVE_STORAGE = 'REMOVE_STORAGE'
+
 export function addStorage(item) {
-  return {type: 'ADD_STORAGE', item }
+  return {type: ADD_STORAGE, item }
 }
 
 export function removeStorage(item) {
-  return {type: 'REMOVE_STORAGE', item}
+  return {type: REMOVE_STORAGE, item}
 }
